629817702cc209dd21590e35184326e1b758902e
Marina Alisionak <alisionok2004@mail.ru>
2023-02-14 20:09:19 +0300

    F

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('.menu-burger__header').click(function(){
         $('.menu-burger__header').toggleClass('open-menu');
-        $('.header__nav').toggleClass('open-menu');
+        $('.header_nav').toggleClass('open-menu');
         $('body').toggleClass('fixed-page');
 	});
 });
